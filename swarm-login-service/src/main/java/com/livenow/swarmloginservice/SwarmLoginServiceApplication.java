9045af458497f19e691a49d0e1f7ae89232b8495
livenow14 <xowjd41@naver.com>
2021-09-27 11:31:39 +0900

    Initialize commit: java application with spring

---
+++ swarm-login-service/src/main/java/com/livenow/swarmloginservice/SwarmLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.livenow.swarmloginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarmLoginServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwarmLoginServiceApplication.class, args);
+    }
+
+}
---
+++ swarm-login-service/src/test/java/com/livenow/swarmloginservice/SwarmLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenow.swarmloginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarmLoginServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
